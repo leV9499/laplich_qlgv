cd89ae253075746891fb61aedac801ca12888260
leV9499 <133184770+leV9499@users.noreply.github.com>
2024-03-28 12:55:54 +0700

    [tao thu muc]

---
+++ QLGVFunction2/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace QLGVFunction2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pnlMatrix = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.btnSunday = new System.Windows.Forms.Button();
+            this.btnSaturday = new System.Windows.Forms.Button();
+            this.btnFriday = new System.Windows.Forms.Button();
+            this.btnThursday = new System.Windows.Forms.Button();
+            this.btnWednesday = new System.Windows.Forms.Button();
+            this.btnTuesday = new System.Windows.Forms.Button();
+            this.btnMonday = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnHomNay = new System.Windows.Forms.Button();
+            this.dpkDate = new System.Windows.Forms.DateTimePicker();
+            this.btnPostMonth = new System.Windows.Forms.Button();
+            this.btnPreMonth = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(12, 23);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(981, 590);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pnlMatrix);
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Location = new System.Drawing.Point(53, 40);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(877, 547);
+            this.panel2.TabIndex = 0;
+            // 
+            // pnlMatrix
+            // 
+            this.pnlMatrix.Location = new System.Drawing.Point(149, 66);
+            this.pnlMatrix.Name = "pnlMatrix";
+            this.pnlMatrix.Size = new System.Drawing.Size(561, 470);
+            this.pnlMatrix.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.btnPreMonth);
+            this.panel4.Controls.Add(this.btnPostMonth);
+            this.panel4.Controls.Add(this.btnSunday);
+            this.panel4.Controls.Add(this.btnSaturday);
+            this.panel4.Controls.Add(this.btnFriday);
+            this.panel4.Controls.Add(this.btnThursday);
+            this.panel4.Controls.Add(this.btnWednesday);
+            this.panel4.Controls.Add(this.btnTuesday);
+            this.panel4.Controls.Add(this.btnMonday);
+            this.panel4.Location = new System.Drawing.Point(3, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(862, 60);
+            this.panel4.TabIndex = 0;
+            // 
+            // btnSunday
+            // 
+            this.btnSunday.Location = new System.Drawing.Point(632, 6);
+            this.btnSunday.Name = "btnSunday";
+            this.btnSunday.Size = new System.Drawing.Size(75, 54);
+            this.btnSunday.TabIndex = 7;
+            this.btnSunday.Text = "Chủ nhật";
+            this.btnSunday.UseVisualStyleBackColor = true;
+            // 
+            // btnSaturday
+            // 
+            this.btnSaturday.Location = new System.Drawing.Point(551, 6);
+            this.btnSaturday.Name = "btnSaturday";
+            this.btnSaturday.Size = new System.Drawing.Size(75, 54);
+            this.btnSaturday.TabIndex = 6;
+            this.btnSaturday.Text = "Thứ 7";
+            this.btnSaturday.UseVisualStyleBackColor = true;
+            // 
+            // btnFriday
+            // 
+            this.btnFriday.Location = new System.Drawing.Point(470, 6);
+            this.btnFriday.Name = "btnFriday";
+            this.btnFriday.Size = new System.Drawing.Size(75, 54);
+            this.btnFriday.TabIndex = 5;
+            this.btnFriday.Text = "Thứ 6";
+            this.btnFriday.UseVisualStyleBackColor = true;
+            // 
+            // btnThursday
+            // 
+            this.btnThursday.Location = new System.Drawing.Point(389, 6);
+            this.btnThursday.Name = "btnThursday";
+            this.btnThursday.Size = new System.Drawing.Size(75, 54);
+            this.btnThursday.TabIndex = 4;
+            this.btnThursday.Text = "Thứ 5";
+            this.btnThursday.UseVisualStyleBackColor = true;
+            // 
+            // btnWednesday
+            // 
+            this.btnWednesday.Location = new System.Drawing.Point(308, 6);
+            this.btnWednesday.Name = "btnWednesday";
+            this.btnWednesday.Size = new System.Drawing.Size(75, 54);
+            this.btnWednesday.TabIndex = 3;
+            this.btnWednesday.Text = "Thứ 4";
+            this.btnWednesday.UseVisualStyleBackColor = true;
+            // 
+            // btnTuesday
+            // 
+            this.btnTuesday.Location = new System.Drawing.Point(227, 6);
+            this.btnTuesday.Name = "btnTuesday";
+            this.btnTuesday.Size = new System.Drawing.Size(75, 54);
+            this.btnTuesday.TabIndex = 1;
+            this.btnTuesday.Text = "Thứ 3";
+            this.btnTuesday.UseVisualStyleBackColor = true;
+            // 
+            // btnMonday
+            // 
+            this.btnMonday.Location = new System.Drawing.Point(146, 6);
+            this.btnMonday.Name = "btnMonday";
+            this.btnMonday.Size = new System.Drawing.Size(75, 54);
+            this.btnMonday.TabIndex = 0;
+            this.btnMonday.Text = "Thứ 2";
+            this.btnMonday.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.btnHomNay);
+            this.panel3.Controls.Add(this.dpkDate);
+            this.panel3.Location = new System.Drawing.Point(12, 26);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(930, 31);
+            this.panel3.TabIndex = 1;
+            // 
+            // btnHomNay
+            // 
+            this.btnHomNay.Location = new System.Drawing.Point(546, 3);
+            this.btnHomNay.Name = "btnHomNay";
+            this.btnHomNay.Size = new System.Drawing.Size(117, 23);
+            this.btnHomNay.TabIndex = 1;
+            this.btnHomNay.Text = "Hôm nay";
+            this.btnHomNay.UseVisualStyleBackColor = true;
+            // 
+            // dpkDate
+            // 
+            this.dpkDate.Location = new System.Drawing.Point(340, 3);
+            this.dpkDate.Name = "dpkDate";
+            this.dpkDate.Size = new System.Drawing.Size(200, 22);
+            this.dpkDate.TabIndex = 0;
+            // 
+            // btnPostMonth
+            // 
+            this.btnPostMonth.Location = new System.Drawing.Point(713, 3);
+            this.btnPostMonth.Name = "btnPostMonth";
+            this.btnPostMonth.Size = new System.Drawing.Size(149, 54);
+            this.btnPostMonth.TabIndex = 8;
+            this.btnPostMonth.Text = "Tháng sau";
+            this.btnPostMonth.UseVisualStyleBackColor = true;
+            // 
+            // btnPreMonth
+            // 
+            this.btnPreMonth.Location = new System.Drawing.Point(3, 6);
+            this.btnPreMonth.Name = "btnPreMonth";
+            this.btnPreMonth.Size = new System.Drawing.Size(140, 54);
+            this.btnPreMonth.TabIndex = 9;
+            this.btnPreMonth.Text = "Tháng trước";
+            this.btnPreMonth.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 611);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.FlowLayoutPanel pnlMatrix;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.DateTimePicker dpkDate;
+        private System.Windows.Forms.Button btnHomNay;
+        private System.Windows.Forms.Button btnSunday;
+        private System.Windows.Forms.Button btnSaturday;
+        private System.Windows.Forms.Button btnFriday;
+        private System.Windows.Forms.Button btnThursday;
+        private System.Windows.Forms.Button btnWednesday;
+        private System.Windows.Forms.Button btnTuesday;
+        private System.Windows.Forms.Button btnMonday;
+        private System.Windows.Forms.Button btnPreMonth;
+        private System.Windows.Forms.Button btnPostMonth;
+    }
+}
+
---
+++ QLGVFunction2/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLGVFunction2
+{
+    public partial class Form1 : Form
+    {
+        private List<List<Button>> matrix;
+        private List<string> dateOfWeek = new List<string>() { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+        public Form1()
+        {
+            InitializeComponent();
+            LoadMatrix();
+        }
+        void LoadMatrix()
+        {
+            matrix = new List<List<Button>>();
+            Button oldBtn = new Button() { Width = 0, Height = 0, Location = new Point(-Cons.margin, 0) };
+            for (int i = 0; i <Cons.DayOfColumn; i++)
+            {
+                matrix.Add(new List<Button>());
+                for (int j = 0; j < Cons.DayOfWeek; j++)
+                {
+                    Button btn = new Button() { Width = Cons.dateButtonWidth, Height = Cons.dateButtonHeight };
+                    btn.Location = new Point(oldBtn.Location.X + oldBtn.Width +Cons.margin, oldBtn.Location.Y);
+                   
+                    pnlMatrix.Controls.Add(btn);
+                    matrix[i].Add(btn);
+                    oldBtn = btn;
+                }
+                oldBtn = new Button() { Width = 0, Height = 0, Location = new Point(-Cons.margin, oldBtn.Location.Y + Cons.dateButtonHeight) };
+            }
+            AddNumberToMatrixByDate(dpkDate.Value);
+        }
+        int DayOfMonth(DateTime date)
+        {
+            switch (date.Month)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    return 31;
+                case 2:
+                    if((date.Year%4==0 && date.Year % 100 != 0) || date.Year % 400 == 0)
+                        return 29; 
+                    else 
+                        return 28; 
+                    break;
+                default:
+                    return 30;
+            }
+        }
+        void AddNumberToMatrixByDate(DateTime date)
+        {
+            DateTime useDate = new DateTime(date.Year, date.Month, 1);
+            int line = 0;
+
+            for (int i = 1; i <= DayOfMonth(date); i++)
+            {
+                int column = dateOfWeek.IndexOf(useDate.DayOfWeek.ToString());
+                //if (column != -1)
+                //{
+                    Button btn = matrix[line][column];
+                    btn.Text = i.ToString();
+                    if (column >= 6)
+                        line++;
+                //}
+                useDate = useDate.AddDays(1);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
